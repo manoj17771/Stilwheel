507e916a2225c1e86211c599e81981fe5acfbd6c
manoj17771 <manojmishra17771@gmail.com>
2019-09-16 11:51:32 +0530

    modified project

--- StiwheelJavaProject/src/Login.java
+++ StiwheelJavaProject/src/Login.java
@@ -4,7 +4,7 @@ public class Login {
 	public static void main(String[] args) {
 		System.out.println("Hello Stiwheel");
 		
-		system.out.println("Added by Usama");
+		System.out.println("Added by Usama");
 
 	}
 
